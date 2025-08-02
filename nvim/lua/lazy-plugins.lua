17a984c9f02d3b4f53db6836067e58e44c5a1105
muleyuck <takuty.008.awenite.1121@gmail.com>
2025-08-02 10:02:08 +0900

    remove: copilot plugin

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -80,12 +80,6 @@ require('lazy').setup({
   require 'kickstart.plugins.hop',
 
   require 'kickstart.plugins.trim',
-
-  require 'kickstart.plugins.copilot',
-
-  require 'kickstart.plugins.copilot-cmp',
-
-  require 'kickstart.plugins.copilot-chat',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
