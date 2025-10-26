2e7a19328fff7ed197900d3c7589a802e08b3840
muleyuck <takuty.008.awenite.1121@gmail.com>
2025-10-26 22:20:09 +0900

    update: nvim use mypy in venv

--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -14,6 +14,50 @@ return {
         python = { 'mypy' }, -- NOTE: require install pydantic (https://kobe-kosen-robotics.org/folder/Wiki/docs/build/html/user_guide/linux/astronvim.html#lint)
       }
 
+      -- Function to find the appropriate mypy executable
+      local function get_mypy_path()
+        -- Try VIRTUAL_ENV environment variable
+        local venv = os.getenv 'VIRTUAL_ENV'
+        if venv and venv ~= '' then
+          local venv_mypy = venv .. '/bin/mypy'
+          if vim.fn.executable(venv_mypy) == 1 then
+            return venv_mypy
+          end
+        end
+
+        -- Try .venv in current directory
+        local cwd = vim.fn.getcwd()
+        local venv_mypy = cwd .. '/.venv/bin/mypy'
+        if vim.fn.executable(venv_mypy) == 1 then
+          return venv_mypy
+        end
+
+        -- Try .venv in git root
+        local git_root_cmd = vim.fn.systemlist 'git rev-parse --show-toplevel 2>/dev/null'
+        if git_root_cmd and #git_root_cmd > 0 then
+          local git_root = git_root_cmd[1]
+          venv_mypy = git_root .. '/.venv/bin/mypy'
+          if vim.fn.executable(venv_mypy) == 1 then
+            return venv_mypy
+          end
+        end
+
+        -- Fallback to system mypy
+        return 'mypy'
+      end
+
+      -- Configure mypy to use virtual environment
+      -- Start with default mypy linter and override cmd
+      lint.linters.mypy.cmd = get_mypy_path()
+
+      -- Update mypy path when entering Python files or changing directory
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'DirChanged' }, {
+        pattern = '*.py',
+        callback = function()
+          lint.linters.mypy.cmd = get_mypy_path()
+        end,
+      })
+
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
       -- lint.linters_by_ft = lint.linters_by_ft or {}
